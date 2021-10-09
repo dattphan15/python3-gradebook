a5916c66cc66d082ee3d96448754cc6b0655f08e
Kevin Phan <ken.phan@live.ca>
2021-10-09 13:56:51 -0700

    modifying gradebook

--- script.py
+++ script.py
@@ -11,10 +11,31 @@ gradebook = [
   ["history", 88]
 ]
 
+print("1. Original List")
 print(gradebook)
+print("\n")
 
 gradebook.append(["computer science", 100])
+print("2. Add Computer Science to List")
 print(gradebook)
+print("\n")
 
 gradebook.append(["visual arts", 93])
+print("3. Add Visual Arts to List")
 print(gradebook)
+print("\n")
+
+gradebook[-1][-1] = 98
+print("4. Modify grade for Visual Arts")
+print(gradebook)
+print("\n")
+
+gradebook[2].remove(85)
+print("5. Remove numerical grade from Poetry")
+print(gradebook)
+print("\n")
+
+gradebook[2].append("Pass")
+print("6. Append Pass grade to Poetry")
+print(gradebook)
+print("\n")
